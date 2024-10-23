66792cd0bcd562a3299409990e5962706d6d86ab
Ephyz8 <ziwoyaephraim@gmail.com>
2024-10-23 18:06:23 +0200

    Login settings

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _ 
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """
+    Custom UserAdmin class to manage the display and behavior of the User model
+    in the Django admin interface. It customizes list views, search functionality, 
+    and the forms used for user creation and change.
+    """
+    ordering = ["email"]
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ["email", "first_name", "last_name", "is_staff", "is_active"]
+    list_display_links = ["email"]
+    list_filter = ["email", "first_name", "last_name", "is_staff", "is_active"]
+    search_fields = ["email", "first_name", "last_name"]
+
+    fieldsets = (
+        (_("Login Credentials"), {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("first_name", "last_name")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "first_name", "last_name", "password1", "password2"),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext as _ 
+from .managers import CustomUserManager
+from django.utils import timezone
 
 # Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name= models.CharField(_("First Name"), max_length=150)
+    last_name= models.CharField(_("Last Name"), max_length=150)
+    email= models.EmailField(_("Email Adress"), max_length=254, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+    
+    objects = CustomUserManager()
+    
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+        
+    def __str__(self):
+        return str(self.email)
+    
+    @property
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
